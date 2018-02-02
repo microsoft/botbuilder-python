f6c829047cb6b33daf60193248a578ee8d13146d
Pablo Costantini <sixdemons@gmail.com>
2018-02-02 17:48:05 -0300

    Update generateClient.cmd - move files to their proper packages

--- libraries/swagger/generateClient.cmd
+++ libraries/swagger/generateClient.cmd
@@ -1,1 +1,12 @@
+@echo off
+
+rd /s /q generated
 call autorest README.md --python
+
+rd /s /q ..\botbuilder-schema\microsoft\botbuilder\schema
+rd /s /q ..\botframework-connector\microsoft\botframework\connector\operations
+del ..\botframework-connector\microsoft\botframework\connector\connector_client.py
+
+move generated\botframework\connector\models ..\botbuilder-schema\microsoft\botbuilder\schema
+move generated\botframework\connector\operations ..\botframework-connector\microsoft\botframework\connector\operations
+move generated\botframework\connector\connector_client.py ..\botframework-connector\microsoft\botframework\connector\connector_client.py
