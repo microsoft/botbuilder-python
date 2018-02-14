93dfb1f6be7d7fd1826e50148aee519c41f16673
Steven Gum <steven.gum@gmail.com>
2018-02-13 16:32:27 -0800

    add echo connector sample and README.md

---
+++ samples/Echo.Connector.Bot/README.md
@@ -0,0 +1,12 @@
+#### Echo Connector Bot
+
+Requires [Flask][Flask] to run.
+
+- To run the sample on Windows, enter the following into your command line:
+
+```
+set FLASK_APP=main.py
+flask run
+```
+
+  [Flask]: http://flask.pocoo.org/
---
+++ samples/Echo.Connector.Bot/main.py
@@ -0,0 +1,42 @@
+import json
+
+from flask import Flask, request, Response
+from microsoft.botframework.connector import ConnectorClient, MicrosoftTokenAuthentication
+from microsoft.botbuilder.schema import *
+
+APP_ID = ''
+APP_PW = ''
+
+app = Flask(__name__)
+app.connector = None
+app.credentials = MicrosoftTokenAuthentication(APP_ID, APP_PW)
+
+
+@app.route("/api/messages", methods=['Post'])
+def messages():
+    activity = json.loads(request.data)
+    if app.connector is None:
+        app.connector = ConnectorClient(app.credentials, base_url=activity['serviceUrl'])
+
+    if activity['type'] == 'message':
+        response_activity = Activity(
+            type=ActivityTypes.message,
+            channel_id=activity['channelId'],
+            recipient=ChannelAccount(id=activity['from']['id'], name=activity['from']['name']),
+            from_property=ChannelAccount(id=activity['recipient']['id'], name=activity['recipient']['name']),
+            text='You said "%s"' % activity['text']
+            )
+        app.connector.conversations.send_to_conversation(activity['conversation']['id'], response_activity)
+        return Response("{}", status=200, mimetype='application/json')
+    elif activity['type'] == 'conversationUpdate':
+        response_activity = Activity(
+            type=ActivityTypes.message,
+            channel_id=activity['channelId'],
+            recipient=ChannelAccount(id=activity['from']['id'], name=activity['from']['name']),
+            from_property=ChannelAccount(id=activity['recipient']['id'], name=activity['recipient']['name']),
+            text='Conversation Update!'
+            )
+        app.connector.conversations.send_to_conversation(activity['conversation']['id'], response_activity)
+        return Response("{}", status=202, mimetype='application/json')
+    else:
+        return Response("{}", status=202, mimetype='application/json')
