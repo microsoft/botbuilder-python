288be9bc70ed464c5c182659c9339e1816046e00
Amit Stein <amit.stein@southworks.com>
2018-02-16 18:00:49 -0300

    add test 'auth header with different bot app id should not validate'

--- libraries/botframework-connector/tests/test_auth.py
+++ libraries/botframework-connector/tests/test_auth.py
@@ -18,3 +18,10 @@ class TestAuth():
         except:
             pytest.fail("Unexpected error")
 
+    @pytest.mark.asyncio
+    async def test_connector_auth_header_with_different_bot_app_id_should_not_validate(self):
+        activity = Activity(service_url = 'https://webchat.botframework.com/') 
+        header = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkdDeEFyWG9OOFNxbzdQd2VBNy16NjVkZW5KUSIsIng1dCI6IkdDeEFyWG9OOFNxbzdQd2VBNy16NjVkZW5KUSJ9.eyJzZXJ2aWNldXJsIjoiaHR0cHM6Ly93ZWJjaGF0LmJvdGZyYW1ld29yay5jb20vIiwiaXNzIjoiaHR0cHM6Ly9hcGkuYm90ZnJhbWV3b3JrLmNvbSIsImF1ZCI6IjM5NjE5YTU5LTVhMGMtNGY5Yi04N2M1LTgxNmM2NDhmZjM1NyIsImV4cCI6MTUxNjczNzUyMCwibmJmIjoxNTE2NzM2OTIwfQ.TBgpxbDS-gx1wm7ldvl7To-igfskccNhp-rU1mxUMtGaDjnsU--usH4OXZfzRsZqMlnXWXug_Hgd_qOr5RH8wVlnXnMWewoZTSGZrfp8GOd7jHF13Gz3F1GCl8akc3jeK0Ppc8R_uInpuUKa0SopY0lwpDclCmvDlz4PN6yahHkt_666k-9UGmRt0DDkxuYjbuYG8EDZxyyAhr7J6sFh3yE2UGRpJjRDB4wXWqv08Cp0Gn9PAW2NxOyN8irFzZH5_YZqE3DXDAYZ_IOLpygXQR0O-bFIhLDVxSz6uCeTBRjh8GU7XJ_yNiRDoaby7Rd2IfRrSnvMkBRsB8MsWN8oXg'
+        credentials = SimpleCredentialProvider('00000000-0000-0000-0000-000000000000', '')
+        with pytest.raises(Exception):
+            await JwtTokenValidation.assert_valid_activity(activity, header, credentials)
