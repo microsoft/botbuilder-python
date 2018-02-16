e88394a4f0d882f4ceb2b72d7fbe98fc8d0e129d
Amit Stein <amit.stein@southworks.com>
2018-02-16 17:29:41 -0300

    Add Test Auth

---
+++ libraries/botframework-connector/tests/test_auth.py
@@ -0,0 +1,20 @@
+import pytest
+
+from microsoft.botbuilder.schema import Activity
+from microsoft.botframework.connector.auth import JwtTokenValidation
+from microsoft.botframework.connector.auth import SimpleCredentialProvider
+
+import asyncio
+
+class TestAuth():
+    
+    @pytest.mark.asyncio
+    async def test_connector_auth_header_correct_app_id_and_service_url_should_validate(self):
+        activity = Activity(service_url = 'https://webchat.botframework.com/') 
+        header = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkdDeEFyWG9OOFNxbzdQd2VBNy16NjVkZW5KUSIsIng1dCI6IkdDeEFyWG9OOFNxbzdQd2VBNy16NjVkZW5KUSJ9.eyJzZXJ2aWNldXJsIjoiaHR0cHM6Ly93ZWJjaGF0LmJvdGZyYW1ld29yay5jb20vIiwiaXNzIjoiaHR0cHM6Ly9hcGkuYm90ZnJhbWV3b3JrLmNvbSIsImF1ZCI6IjM5NjE5YTU5LTVhMGMtNGY5Yi04N2M1LTgxNmM2NDhmZjM1NyIsImV4cCI6MTUxNjczNzUyMCwibmJmIjoxNTE2NzM2OTIwfQ.TBgpxbDS-gx1wm7ldvl7To-igfskccNhp-rU1mxUMtGaDjnsU--usH4OXZfzRsZqMlnXWXug_Hgd_qOr5RH8wVlnXnMWewoZTSGZrfp8GOd7jHF13Gz3F1GCl8akc3jeK0Ppc8R_uInpuUKa0SopY0lwpDclCmvDlz4PN6yahHkt_666k-9UGmRt0DDkxuYjbuYG8EDZxyyAhr7J6sFh3yE2UGRpJjRDB4wXWqv08Cp0Gn9PAW2NxOyN8irFzZH5_YZqE3DXDAYZ_IOLpygXQR0O-bFIhLDVxSz6uCeTBRjh8GU7XJ_yNiRDoaby7Rd2IfRrSnvMkBRsB8MsWN8oXg'
+        credentials = SimpleCredentialProvider('39619a59-5a0c-4f9b-87c5-816c648ff357', '')
+        try:
+            await JwtTokenValidation.assert_valid_activity(activity, header, credentials)
+        except:
+            pytest.fail("Unexpected error")
+
