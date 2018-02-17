e245b28c95055f43426e2dbbb74570bc113f7444
John Taylor <johtaylo@microsoft.com>
2018-02-16 18:03:58 -0800

    simplify echo connector sample

--- samples/Echo.Connector.Bot/main.py
+++ samples/Echo.Connector.Bot/main.py
@@ -1,50 +1,62 @@
-import json
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
-from flask import Flask, request, Response
+import http.server
+import json
+from microsoft.botbuilder.schema import *
 from microsoft.botframework.connector import ConnectorClient
 from microsoft.botframework.connector.auth import MicrosoftTokenAuthentication
-from microsoft.botbuilder.schema import *
 
 APP_ID = ''
-APP_PW = ''
+APP_PASSWORD = ''
 
-app = Flask(__name__)
-app.connector = None
-app.credentials = MicrosoftTokenAuthentication(APP_ID, APP_PW)
+class MyHandler(http.server.BaseHTTPRequestHandler):
 
+    def handle_conversationUpdate_activity(self, data):
+        self.send_response(202)
+        self.end_headers()
+        if data['membersAdded'][0]['id'] != data['recipient']['id']:
+            credentials = MicrosoftTokenAuthentication(APP_ID, APP_PASSWORD)
+            connector = ConnectorClient(credentials, base_url=data['serviceUrl'])
+            activity = Activity(
+                type=ActivityTypes.message,
+                channel_id=data['channelId'],
+                recipient=ChannelAccount(id=data['from']['id'], name=data['from']['name']),
+                from_property=ChannelAccount(id=data['recipient']['id'], name=data['recipient']['name']),
+                text='Hello and welcome to the echo bot!')
+            connector.conversations.send_to_conversation(data['conversation']['id'], activity)
 
-@app.route("/api/messages", methods=['Post'])
-def messages():
-    activity = json.loads(request.data)
-    if app.connector is None:
-        app.connector = ConnectorClient(app.credentials, base_url=activity['serviceUrl'])
+    def handle_message_activity(self, data):
+        self.send_response(200)
+        self.end_headers()
+        credentials = MicrosoftTokenAuthentication(APP_ID, APP_PASSWORD)
+        connector = ConnectorClient(credentials, base_url=data['serviceUrl'])
+        activity = Activity(
+            type = ActivityTypes.message,
+            channel_id = data['channelId'],
+            recipient = ChannelAccount(id=data['from']['id']),
+            from_property = ChannelAccount(id=data['recipient']['id']),
+            text = 'You said: %s' % data['text'])
+        connector.conversations.send_to_conversation(data['conversation']['id'], activity)
 
-    if activity['type'] == 'message':
-        response_activity = Activity(
-            type=ActivityTypes.message,
-            channel_id=activity['channelId'],
-            recipient=ChannelAccount(id=activity['from']['id'], name=activity['from']['name']),
-            from_property=ChannelAccount(id=activity['recipient']['id'], name=activity['recipient']['name']),
-            text='You said "%s"' % activity['text']
-            )
-        app.connector.conversations.send_to_conversation(activity['conversation']['id'], response_activity)
-        return Response("{}", status=200, mimetype='application/json')
-    elif activity['type'] == 'conversationUpdate':
-        message = 'Conversation update!'
-        if 'membersAdded' in activity:
-            message = 'Conversation update!\n\n"%s" (id: %s) joined the conversation.' %\
-                      (activity['membersAdded'][0]['name'],
-                       activity['membersAdded'][0]['id'])
-        elif 'membersRemoved' in activity:
-            message = 'Conversation update!\n\nid: "%s" left the conversation.' % activity['membersRemoved'][0]['id']
-        response_activity = Activity(
-            type=ActivityTypes.message,
-            channel_id=activity['channelId'],
-            recipient=ChannelAccount(id=activity['from']['id'], name=activity['from']['name']),
-            from_property=ChannelAccount(id=activity['recipient']['id'], name=activity['recipient']['name']),
-            text=message
-            )
-        app.connector.conversations.send_to_conversation(activity['conversation']['id'], response_activity)
-        return Response("{}", status=202, mimetype='application/json')
-    else:
-        return Response("{}", status=202, mimetype='application/json')
+    def unhandled_activity(self):
+        self.send_response(404)
+        self.end_headers()
+
+    def do_POST(self):
+        body = self.rfile.read(int(self.headers['Content-Length']))
+        data = json.loads(str(body, 'utf-8'))
+        if data['type'] == 'conversationUpdate':
+            self.handle_conversationUpdate_activity(data)
+        elif data['type'] == 'message':
+            self.handle_message_activity(data)
+        else:
+            self.unhandled_activity()
+
+try:
+    server = http.server.HTTPServer(('localhost', 9000), MyHandler)
+    print('Started http server')
+    server.serve_forever()
+except KeyboardInterrupt:
+    print('^C received, shutting down server')
+    server.socket.close()
