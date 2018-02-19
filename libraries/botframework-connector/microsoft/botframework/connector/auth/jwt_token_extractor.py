59c6ed3dc935f9921e1a4513ad4dbf21ef1fcc73
JuanAr <juan.arguello@southworks.com>
2018-02-19 16:15:45 -0300

    Fixed pylint warnings jwt_token_extractor

--- libraries/botframework-connector/microsoft/botframework/connector/auth/jwt_token_extractor.py
+++ libraries/botframework-connector/microsoft/botframework/connector/auth/jwt_token_extractor.py
@@ -1,32 +1,32 @@
 import asyncio
-import requests
-import jwt
 import json
-from jwt.algorithms import RSAAlgorithm
 from datetime import datetime, timedelta
+import requests
+from jwt.algorithms import RSAAlgorithm
+import jwt
 from .claims_identity import ClaimsIdentity
 
 class JwtTokenExtractor:
     metadataCache = {}
 
-    def __init__(self, validationParams, metadataUrl, allowedAlgorithms, validator = None):
-        self.validationParameters = validationParams
-        self.validationParameters.algorithms = allowedAlgorithms
-        self.openIdMetadata = JwtTokenExtractor.get_open_id_metadata(metadataUrl)
-        self.validator = validator if validator is not None else lambda x : True
+    def __init__(self, validationParams, metadata_url, allowedAlgorithms, validator=None):
+        self.validation_parameters = validationParams
+        self.validation_parameters.algorithms = allowedAlgorithms
+        self.open_id_metadata = JwtTokenExtractor.get_open_id_metadata(metadata_url)
+        self.validator = validator if validator is not None else lambda x: True
 
     @staticmethod
-    def get_open_id_metadata(metadataUrl):
-        metadata = JwtTokenExtractor.metadataCache.get(metadataUrl, None)
+    def get_open_id_metadata(metadata_url):
+        metadata = JwtTokenExtractor.metadataCache.get(metadata_url, None)
         if metadata is None:
-            metadata = _OpenIdMetadata(metadataUrl)
-            JwtTokenExtractor.metadataCache.setdefault(metadataUrl, metadata)
+            metadata = _OpenIdMetadata(metadata_url)
+            JwtTokenExtractor.metadataCache.setdefault(metadata_url, metadata)
         return metadata
 
-    async def get_identity_from_auth_header(self, authHeader):
-        if not authHeader:
+    async def get_identity_from_auth_header(self, auth_header):
+        if not auth_header:
             return None
-        parts = authHeader.split(" ")
+        parts = auth_header.split(" ")
         if len(parts) == 2:
             return await self.get_identity(parts[0], parts[1])
         return None
@@ -35,42 +35,44 @@ class JwtTokenExtractor:
         # No header in correct scheme or no token
         if schema != "Bearer" or not parameter:
             return None
-        
+
         # Issuer isn't allowed? No need to check signature
         if not self._has_allowed_issuer(parameter):
             return None
-        
+
         try:
             return await self._validate_token(parameter)
         except:
             raise
 
-    def _has_allowed_issuer(self, jwtToken):
-        decoded = jwt.decode(jwtToken, verify=False)
+    def _has_allowed_issuer(self, jwt_token):
+        decoded = jwt.decode(jwt_token, verify=False)
         issuer = decoded.get("iss", None)
-        if issuer in self.validationParameters.issuer:
+        if issuer in self.validation_parameters.issuer:
             return True
-        
-        return issuer is self.validationParameters.issuer
 
-    async def _validate_token(self, jwtToken):
-        headers = jwt.get_unverified_header(jwtToken)
+        return issuer is self.validation_parameters.issuer
+
+    async def _validate_token(self, jwt_token):
+        headers = jwt.get_unverified_header(jwt_token)
 
         # Update the signing tokens from the last refresh
-        keyId = headers.get("kid", None)
-        metadata = await self.openIdMetadata.get(keyId)
+        key_id = headers.get("kid", None)
+        metadata = await self.open_id_metadata.get(key_id)
 
 
-        if headers.get("alg", None) not in self.validationParameters.algorithms:
+        if headers.get("alg", None) not in self.validation_parameters.algorithms:
             raise Exception('Token signing algorithm not in allowed list')
 
         if self.validator is not None:
             if not self.validator(metadata.endorsements):
                 raise Exception('Could not validate endorsement key')
-                
-        options = {'verify_aud': False, 'verify_exp': not self.validationParameters.ignore_expiration }
-        decodedPayload = jwt.decode(jwtToken, metadata.publicKey, options=options)
-        claims = ClaimsIdentity(decodedPayload, True)
+
+        options = {
+            'verify_aud': False,
+            'verify_exp': not self.validation_parameters.ignore_expiration}
+        decoded_payload = jwt.decode(jwt_token, metadata.public_key, options=options)
+        claims = ClaimsIdentity(decoded_payload, True)
 
         return claims
 
@@ -78,32 +80,32 @@ class _OpenIdMetadata:
     def __init__(self, url):
         self.url = url
         self.keys = []
-        self.lastUpdated = datetime.min
+        self.last_updated = datetime.min
 
-    async def get(self, keyId):
+    async def get(self, key_id):
         # If keys are more than 5 days old, refresh them
-        if self.lastUpdated < (datetime.now() + timedelta(days=5)):
+        if self.last_updated < (datetime.now() + timedelta(days=5)):
             await self._refresh()
-        return self._find(keyId)
+        return self._find(key_id)
 
     async def _refresh(self):
         response = requests.get(self.url)
         response.raise_for_status()
-        keysUrl = response.json()["jwks_uri"]
-        responseKeys = requests.get(keysUrl)
-        responseKeys.raise_for_status()
-        self.lastUpdated = datetime.now()
-        self.keys = responseKeys.json()["keys"]
+        keys_url = response.json()["jwks_uri"]
+        response_keys = requests.get(keys_url)
+        response_keys.raise_for_status()
+        self.last_updated = datetime.now()
+        self.keys = response_keys.json()["keys"]
 
-    def _find(self, keyId):
-        if  len(self.keys) == 0:
+    def _find(self, key_id):
+        if not self.keys:
             return None
-        key = next(x for x in self.keys if x["kid"] == keyId)
-        publicKey = RSAAlgorithm.from_jwk(json.dumps(key))
+        key = next(x for x in self.keys if x["kid"] == key_id)
+        public_key = RSAAlgorithm.from_jwk(json.dumps(key))
         endorsements = key.get("endorsements", [])
-        return _OpenIdConfig(publicKey, endorsements)
-        
+        return _OpenIdConfig(public_key, endorsements)
+
 class _OpenIdConfig:
-    def __init__(self, publicKey, endorsements):
-        self.publicKey = publicKey
+    def __init__(self, public_key, endorsements):
+        self.public_key = public_key
         self.endorsements = endorsements
