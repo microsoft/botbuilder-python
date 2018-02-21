7696949cf5b1f8abc06cc86e32517ed5acc7b322
Steven Gum <steven.gum@gmail.com>
2018-02-20 16:56:14 -0800

    fix Echo.Connector.Bot.Adapter sample, add types and meet naming conventions

--- samples/Echo.Connector.Bot.Adapter/receive_delegate.py
+++ samples/Echo.Connector.Bot.Adapter/receive_delegate.py
@@ -1,7 +1,9 @@
+from microsoft.botbuilder.schema import Activity
+
 
 class ReceiveDelegate:
     def __init__(self, target):
         self._target = target
 
-    def call(self, activity):
+    def call(self, activity: Activity):
         self._target.receive(activity)
