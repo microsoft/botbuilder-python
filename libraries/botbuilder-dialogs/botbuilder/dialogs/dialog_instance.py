03f5a753e63633c101e96ffe78ffbb0e8660a4cc
congysu <congysu@users.noreply.github.com>
2019-04-15 16:09:50 -0700

    port for dialog instance

---
+++ libraries/botbuilder-dialogs/botbuilder/dialogs/dialog_instance.py
@@ -0,0 +1,53 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from typing import Dict
+
+
+class DialogInstance:
+    """
+    Tracking information for a dialog on the stack.
+    """
+
+    def __init__(self):
+        self._id: str = None
+        self._state: Dict[str, object] = {}
+
+    @property
+    def id(self) -> str:
+        """Gets the ID of the dialog this instance is for.
+
+        :param:
+        :return str:
+        """
+        return self._id
+
+    @id.setter
+    def id(self, value: str) -> None:
+        """Sets the ID of the dialog this instance is for.
+
+        :param:
+        :param value: ID of the dialog this instance is for.
+        :return:
+        """
+        self._id = value
+
+    @property
+    def state(self) -> Dict[str, object]:
+        """Gets the instance's persisted state.
+
+        :param:
+        :return Dict[str, object]:
+        """
+
+        return self._state
+
+    @state.setter
+    def state(self, value: Dict[str, object]) -> None:
+        """Sets the instance's persisted state.
+
+        :param:
+        :param value: The instance's persisted state.
+        :return:
+        """
+        self._state = value
