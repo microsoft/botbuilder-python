ade02a84ca7dc13f7a575f2d842497b6719e10cb
Axel Suarez <axsuarez@microsoft.com>
2019-04-25 18:09:13 -0700

    sample done, yaml configand testing missing

--- samples/Core-Bot/main.py
+++ samples/Core-Bot/main.py
@@ -11,6 +11,10 @@ from botbuilder.schema import (Activity, ActivityTypes)
 from botbuilder.core import (BotFrameworkAdapter, BotFrameworkAdapterSettings, TurnContext,
                              ConversationState, MemoryStorage, UserState)
 
+from dialogs import MainDialog
+from bots import DialogAndWelcomeBot
+from helpers.dialog_helper import DialogHelper
+
 APP_ID = ''
 APP_PASSWORD = ''
 PORT = 9000
@@ -19,73 +23,25 @@ ADAPTER = BotFrameworkAdapter(SETTINGS)
 
 # Create MemoryStorage, UserState and ConversationState
 memory = MemoryStorage()
-# Commented out user_state because it's not being used.
-# user_state = UserState(memory)
+
+user_state = UserState(memory)
 conversation_state = ConversationState(memory)
 
-# Register both State middleware on the adapter.
-# Commented out user_state because it's not being used.
-# ADAPTER.use(user_state)
-ADAPTER.use(conversation_state)
+dialog = MainDialog({})
+bot = DialogAndWelcomeBot(conversation_state, user_state, dialog)
 
-
-async def create_reply_activity(request_activity, text) -> Activity:
-    return Activity(
-        type=ActivityTypes.message,
-        channel_id=request_activity.channel_id,
-        conversation=request_activity.conversation,
-        recipient=request_activity.from_property,
-        from_property=request_activity.recipient,
-        text=text,
-        service_url=request_activity.service_url)
-
-
-async def handle_message(context: TurnContext) -> web.Response:
-    # Access the state for the conversation between the user and the bot.
-    state = await conversation_state.get(context)
-
-    if hasattr(state, 'counter'):
-        state.counter += 1
-    else:
-        state.counter = 1
-
-    response = await create_reply_activity(context.activity, f'{state.counter}: You said {context.activity.text}.')
-    await context.send_activity(response)
-    return web.Response(status=202)
-
-
-async def handle_conversation_update(context: TurnContext) -> web.Response:
-    if context.activity.members_added[0].id != context.activity.recipient.id:
-        response = await create_reply_activity(context.activity, 'Welcome to the Echo Adapter Bot!')
-        await context.send_activity(response)
-    return web.Response(status=200)
-
-
-async def unhandled_activity() -> web.Response:
-    return web.Response(status=404)
-
-
-async def request_handler(context: TurnContext) -> web.Response:
-    if context.activity.type == 'message':
-        return await handle_message(context)
-    elif context.activity.type == 'conversationUpdate':
-        return await handle_conversation_update(context)
-    else:
-        return await unhandled_activity()
-
-
-async def messages(req: web.web_request) -> web.Response:
+async def messages(req: web.Request) -> web.Response:
     body = await req.json()
     activity = Activity().deserialize(body)
     auth_header = req.headers['Authorization'] if 'Authorization' in req.headers else ''
     try:
-        return await ADAPTER.process_activity(activity, auth_header, request_handler)
+        return await ADAPTER.process_activity(activity, auth_header, lambda turn_context: await bot.on_turn(turn_context))
     except Exception as e:
         raise e
 
 
 app = web.Application()
-app.router.add_post('/', messages)
+app.router.add_post('/api/messages', messages)
 
 try:
     web.run_app(app, host='localhost', port=PORT)
