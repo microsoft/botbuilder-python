d8ec97431ab8b0bc82f571700dfe114897680222
Dave Taniguchi <daveta@microsoft.com>
2019-05-02 16:52:44 -0700

    Add recognizers (temporarily)

--- samples/Core-Bot/datatypes_timex_expression/timex_convert.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-class TimexConvert:
-
-    @staticmethod
-    def convert_timex_to_string(timex):
-        return ''
