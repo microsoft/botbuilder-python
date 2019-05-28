f27650a9efe0ecb10a80c3e1764514c32439e59c
Axel Suarez <axsuarez@microsoft.com>
2019-05-28 16:12:11 -0700

    add missing None check for send_activity in turn_context

--- libraries/botbuilder-core/botbuilder/core/turn_context.py
+++ libraries/botbuilder-core/botbuilder/core/turn_context.py
@@ -149,7 +149,7 @@ class TurnContext(object):
 
         result = await self._emit(self._on_send_activities, output, callback(self, output))
 
-        return result[0] if len(result) > 0 else ResourceResponse()
+        return result[0] if result is not None and len(result) > 0 else ResourceResponse()
 
     async def update_activity(self, activity: Activity):
         """
