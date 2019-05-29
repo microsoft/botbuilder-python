da077d12848321a6bc9f347381b582dca06ccf32
Dave Taniguchi <daveta@microsoft.com>
2019-05-29 12:29:09 -0700

    Fix LUIS helper to handle response correctly

--- samples/python-flask/13.core-bot/helpers/luis_helper.py
+++ samples/python-flask/13.core-bot/helpers/luis_helper.py
@@ -1,6 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-
 from botbuilder.ai.luis import LuisRecognizer, LuisApplication
 from botbuilder.core import TurnContext
 
@@ -14,25 +13,33 @@ class LuisHelper:
 
         try:
             luis_application = LuisApplication(
-                configuration['LuisApplication'],
-                configuration['LuisAPIKey'],
-                'https://'+configuration['LuisAPIHostName']
+                configuration['LUIS_APP_ID'],
+                configuration['LUIS_API_KEY'],
+                configuration['LUIS_API_HOST_NAME']
             )
 
             recognizer = LuisRecognizer(luis_application)
             recognizer_result = await recognizer.recognize(turn_context)
 
-            intent = sorted(recognizer_result.intents, key=recognizer_result.intents.get, reverse=True)[:1] if recognizer_result.intents else None
+            if recognizer_result.intents:
+                intent = sorted(recognizer_result.intents, key=recognizer_result.intents.get, reverse=True)[:1][0]
+                if intent == 'Book_flight':
+                    # We need to get the result from the LUIS JSON which at every level returns an array.
+                    to_entities = recognizer_result.entities.get("$instance", {}).get("To", [])
+                    if len(to_entities) > 0:
+                        booking_details.destination = to_entities[0]['text']
+                    from_entities = recognizer_result.entities.get("$instance", {}).get("From", [])
+                    if len(from_entities) > 0:
+                        booking_details.origin = from_entities[0]['text']
 
-            if intent == 'Book_flight':
-                # We need to get the result from the LUIS JSON which at every level returns an array.
-                booking_details.destination = recognizer_result.entities.get("To", {}).get("Airport", [])[:1][:1]
-                booking_details.origin = recognizer_result.entities.get("From", {}).get("Airport", [])[:1][:1]
-
-                # This value will be a TIMEX. And we are only interested in a Date so grab the first result and drop the Time part.
-                # TIMEX is a format that represents DateTime expressions that include some ambiguity. e.g. missing a Year.
-                booking_details.travel_date = recognizer_result.entities.get("datetime", {}).get("timex", [])[:1].split('T')[0]
+                    # TODO: This value will be a TIMEX. And we are only interested in a Date so grab the first result and drop the Time part.
+                    # TIMEX is a format that represents DateTime expressions that include some ambiguity. e.g. missing a Year.
+                    date_entities = recognizer_result.entities.get("$instance", {}).get("datetime", [])
+                    if len(date_entities) > 0:
+                        text = date_entities[0]['text']
+                        booking_details.travel_date = None  # TODO: Set when we get a timex format
         except Exception as e:
             print(e)
         
         return booking_details
+
