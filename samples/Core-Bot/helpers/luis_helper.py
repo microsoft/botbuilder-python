4013475aaac28b2cbc485bf01d0abd881ed3bb65
Tien Suwandy <tiens@microsoft.com>
2019-05-30 10:23:11 -0700

    Fixed up  luis_helper in core-bot sample

--- samples/Core-Bot/helpers/luis_helper.py
+++ samples/Core-Bot/helpers/luis_helper.py
@@ -14,7 +14,7 @@ class LuisHelper:
 
         try:
             luis_application = LuisApplication(
-                configuration['LuisApplication'],
+                configuration['LuisAppId'],
                 configuration['LuisAPIKey'],
                 'https://'+configuration['LuisAPIHostName']
             )
@@ -22,16 +22,23 @@ class LuisHelper:
             recognizer = LuisRecognizer(luis_application)
             recognizer_result = await recognizer.recognize(turn_context)
 
-            intent = sorted(recognizer_result.intents, key=recognizer_result.intents.get, reverse=True)[:1] if recognizer_result.intents else None
+            intent = sorted(recognizer_result.intents, key=recognizer_result.intents.get, reverse=True)[:1][0] if recognizer_result.intents else None
 
             if intent == 'Book_flight':
                 # We need to get the result from the LUIS JSON which at every level returns an array.
-                booking_details.destination = recognizer_result.entities.get("To", {}).get("Airport", [])[:1][:1]
-                booking_details.origin = recognizer_result.entities.get("From", {}).get("Airport", [])[:1][:1]
+                to_entities = recognizer_result.entities.get("$instance", {}).get("To", [])
+                if len(to_entities) > 0:
+                    booking_details.destination = to_entities[0]['text']
 
-                # This value will be a TIMEX. And we are only interested in a Date so grab the first result and drop the Time part.
+                from_entities = recognizer_result.entities.get("$instance", {}).get("From", [])
+                if len(from_entities) > 0:
+                    booking_details.origin = from_entities[0]['text']
+
+                # TODO: This value will be a TIMEX. And we are only interested in a Date so grab the first result and drop the Time part.
                 # TIMEX is a format that represents DateTime expressions that include some ambiguity. e.g. missing a Year.
-                booking_details.travel_date = recognizer_result.entities.get("datetime", {}).get("timex", [])[:1].split('T')[0]
+                date_entities = recognizer_result.entities.get("$instance", {}).get("datetime", [])
+                if len(date_entities) > 0:
+                    booking_details.travel_date = None  # TODO: Set when we get a timex format
         except Exception as e:
             print(e)
         
