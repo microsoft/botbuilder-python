3743a0f8d01a587a1263d5a5907990cc74bda385
Ashley Ho <35248895+Zerryth@users.noreply.github.com>
2019-06-25 13:48:47 -0700

    removed oauth prompt settings to not conflict w/Axel's PR; building out Find class & choice tokenizer

--- libraries/botbuilder-dialogs/botbuilder/dialogs/choices/find.py
+++ libraries/botbuilder-dialogs/botbuilder/dialogs/choices/find.py
@@ -1,16 +1,67 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from typing import Union
+from typing import List, Union
 
 from .choice import Choice
-from .find_choices_options import FindChoicesOptions
+from .find_choices_options import FindChoicesOptions, FindValuesOptions
+from .model_result import ModelResult
+from .sorted_value import SortedValue
 
 class Find:
     """ Contains methods for matching user input against a list of choices """
+    
+    @staticmethod
+    def find_choices(
+        utterance: str,
+        choices: [ Union[str, Choice] ],
+        options: FindChoicesOptions = None
+    ):
+        """ Matches user input against a list of choices """
 
-    def __init__(self, utterance: str, choices: Union[str, Choice], options: FindChoicesOptions = None):
         if not choices:
-            raise TypeError('Find: choices cannot be None.')
+            raise TypeError('Find: choices cannot be None. Must be a [str] or [Choice].')
         
-        self.options = options if options else FindChoicesOptions()
+        opt = options if options else FindChoicesOptions()
+
+        # Normalize list of choices
+        choices_list = [ Choice(value=choice) if isinstance(choice, str) else choice for choice in choices ]
+
+        # Build up full list of synonyms to search over.
+        # - Each entry in the list contains the index of the choice it belongs to which will later be
+        # used to map the search results back to their choice.
+        synonyms: [SortedValue] = []
+
+        for index in range(len(choices_list)):
+            choice = choices_list[index]
+
+            if not opt.no_value:
+                synonyms.append( SortedValue(value=choice.value, index=index) )
+            
+            if getattr(choice, 'action', False) and getattr(choice.action, 'title', False) and not opt.no_value:
+                synonyms.append( SortedValue(value=choice.action.title, index=index) )
+            
+            if choice.synonyms != None:
+                for synonym in synonyms:
+                    synonyms.append( SortedValue(value=synonym, index=index) )
+        
+        # Find synonyms in utterance and map back to their choices_list
+        # WRITE FindValues()!!
+    
+    @staticmethod
+    def _find_values(
+        utterance: str,
+        values: List[SortedValue],
+        options: FindValuesOptions = None
+    ):
+        # Sort values in descending order by length, so that the longest value is searchd over first.
+        sorted_values = sorted(
+            values,
+            key = lambda sorted_val: len(sorted_val.value),
+            reverse = True
+        )
+
+        # Search for each value within the utterance.
+        matches: [ModelResult] = []
+        opt = options if options else FindValuesOptions()
+        # tokenizer = opt.tokenizer if opt.tokenizer else 
--- libraries/botbuilder-dialogs/botbuilder/dialogs/choices/tokenizer.py
+++ libraries/botbuilder-dialogs/botbuilder/dialogs/choices/tokenizer.py
@@ -1,5 +1,29 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+from typing import Union
+
+from .token import Token
+
 class Tokenizer:
     """ Provides a default tokenizer implementation. """
+
+    @staticmethod
+    def default_tokenizer(text: str, locale: str = None) -> [Token]:
+        tokens: [Token] = []
+        token: Union[Token, None] = None
+
+        # Parse text
+        length: int = len(text) if text else 0
+        i: int = 0
+
+        while i < length:
+            # Get botht he UNICODE value of the current character and the complete character itself
+            # which can potentially be multiple segments
+            code_point = ord(text[i])
+            char = chr(code_point)
+
+            # Process current character
+            # WRITE IsBreakingChar(code_point)
+    
+    # def _is
