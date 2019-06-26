dc2db650051e04ebc695a03c38c866087824a713
Ashley Ho <35248895+Zerryth@users.noreply.github.com>
2019-06-26 09:49:21 -0700

    removed note to self

--- libraries/botbuilder-dialogs/botbuilder/dialogs/choices/choice_recognizers.py
+++ libraries/botbuilder-dialogs/botbuilder/dialogs/choices/choice_recognizers.py
@@ -13,15 +13,6 @@ from .model_result import ModelResult
 class ChoiceRecognizers:
     """ Contains methods for matching user input against a list of choices. """
 
-    # Note to self: C# implementation has 2 RecognizeChoices overloads, different in their list parameter
-    # 1. list of strings - that gets converted into a list of Choice's
-    # 2. list of choices
-    # Looks like in TS the implement also allows for either string[] or Choice[]
-
-    # C# none of the functions seem to be nested inside another function
-    # TS has only 1 recognizer funtion, recognizeChoices()
-        # nested within recognizeChoices() is matchChoiceByIndex()
-    
     @staticmethod
     def recognize_choices(
         utterance: str,
