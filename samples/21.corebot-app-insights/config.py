ac61654dc996d023e601a7b5b4bdd96f5ff948a2
Axel Suarez <axsuarez@microsoft.com>
2019-08-07 14:54:29 -0700

    Fixes for daveta review comments.

--- samples/21.corebot-app-insights/config.py
+++ samples/21.corebot-app-insights/config.py
@@ -3,16 +3,17 @@
 # Licensed under the MIT License.
 """Configuration for the bot."""
 
+import os
 
-class DefaultConfig(object):
+
+class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = ""
-    APP_PASSWORD = ""
-    LUIS_APP_ID = ""
-    # LUIS authoring key from LUIS portal or LUIS Cognitive Service subscription key
-    LUIS_API_KEY = ""
-    # LUIS endpoint host name, ie "https://westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = ""
-    APPINSIGHTS_INSTRUMENTATION_KEY = ""
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get("AppInsightsInstrumentationKey", "")
