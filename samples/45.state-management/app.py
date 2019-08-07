f415dddbf960b133a17f66c54664e0a579657230
Axel Suárez <axsuarez@microsoft.com>
2019-08-07 16:00:08 -0700

    Naming samples and porting to Flask (#282)
    * Ported main samples from aiohttp to Flask. Rename and rearrenge of samples to match BotBuilder-Samples

---
+++ samples/45.state-management/app.py
@@ -0,0 +1,87 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""
+This sample shows how to manage state in a bot.
+"""
+import asyncio
+import sys
+
+from flask import Flask, request, Response
+from botbuilder.core import (
+    BotFrameworkAdapter,
+    BotFrameworkAdapterSettings,
+    ConversationState,
+    MemoryStorage,
+    TurnContext,
+    UserState,
+)
+from botbuilder.schema import Activity
+
+from bots import StateManagementBot
+
+LOOP = asyncio.get_event_loop()
+APP = Flask(__name__, instance_relative_config=True)
+APP.config.from_object("config.DefaultConfig")
+
+SETTINGS = BotFrameworkAdapterSettings(
+    APP.config["APP_ID"], APP.config["APP_PASSWORD"]
+)
+ADAPTER = BotFrameworkAdapter(SETTINGS)
+
+# Catch-all for errors.
+async def on_error(context: TurnContext, error: Exception):
+    # This check writes out errors to console log
+    # NOTE: In production environment, you should consider logging this to Azure
+    #       application insights.
+    print(f"\n [on_turn_error]: { error }", file=sys.stderr)
+    # Send a message to the user
+    await context.send_activity("Oops. Something went wrong!")
+    # Clear out state
+    await CONVERSATION_STATE.delete(context)
+
+
+ADAPTER.on_turn_error = on_error
+
+# Create MemoryStorage, UserState and ConversationState
+MEMORY = MemoryStorage()
+
+# Commented out user_state because it's not being used.
+USER_STATE = UserState(MEMORY)
+CONVERSATION_STATE = ConversationState(MEMORY)
+
+
+BOT = StateManagementBot(CONVERSATION_STATE, USER_STATE)
+
+
+@APP.route("/api/messages", methods=["POST"])
+def messages():
+    """Main bot message handler."""
+    if request.headers["Content-Type"] == "application/json":
+        body = request.json
+    else:
+        return Response(status=415)
+
+    activity = Activity().deserialize(body)
+    auth_header = (
+        request.headers["Authorization"] if "Authorization" in request.headers else ""
+    )
+
+    async def aux_func(turn_context):
+        await BOT.on_turn(turn_context)
+
+    try:
+        task = LOOP.create_task(
+            ADAPTER.process_activity(activity, auth_header, aux_func)
+        )
+        LOOP.run_until_complete(task)
+        return Response(status=201)
+    except Exception as exception:
+        raise exception
+
+
+if __name__ == "__main__":
+    try:
+        APP.run(debug=False, port=APP.config["PORT"])  # nosec debug
+    except Exception as exception:
+        raise exception
