65f73d0d52b531bf8437da50773501bf38b53b1b
Axel Suárez <axsuarez@microsoft.com>
2019-09-30 18:14:27 -0700

    Adding Recognizer interface to LuisRecognizer (#326)
    * Adding Recognizer interface to LuisRecognizer
    
    * adding abstract method

--- libraries/botbuilder-core/botbuilder/core/recognizer.py
+++ libraries/botbuilder-core/botbuilder/core/recognizer.py
@@ -1,12 +1,12 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from .turn_context import TurnContext
 from .recognizer_result import RecognizerResult
 
 
 class Recognizer(ABC):
-    @staticmethod
-    async def recognize(turn_context: TurnContext) -> RecognizerResult:
+    @abstractmethod
+    async def recognize(self, turn_context: TurnContext) -> RecognizerResult:
         raise NotImplementedError()
