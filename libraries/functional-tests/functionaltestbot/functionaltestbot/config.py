10fa3bd5d24a68a4fb3a7cabf8b7d8d504bdd6e2
Dave Taniguchi <daveta@microsoft.com>
2019-10-28 13:07:13 -0700

    Ugh, listen on port given

--- libraries/functional-tests/functionaltestbot/functionaltestbot/config.py
+++ libraries/functional-tests/functionaltestbot/functionaltestbot/config.py
@@ -10,6 +10,6 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 3978
+    PORT = os.environ.get("PORT", 3978)
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
