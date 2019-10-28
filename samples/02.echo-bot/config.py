52fe260d9a53632867c8698ab122d5f5befb6d70
Tracy Boehrer <tracy.boehrer@gmail.com>
2019-10-28 16:33:06 -0500

    Removed LUIS keys from settings.

--- samples/02.echo-bot/config.py
+++ samples/02.echo-bot/config.py
@@ -13,7 +13,3 @@ class DefaultConfig(object):
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
-    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
