b4445f45836803b4f810f35600e837df7690055d
Tracy Boehrer <tracy.boehrer@gmail.com>
2019-10-28 13:49:08 -0500

    Added 05.multi-turn-prompt

---
+++ samples/05.multi-turn-prompt/app.py
@@ -0,0 +1,88 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""
+This sample shows how to use a WaterfallDialog to prompt the user.
+"""
+import asyncio
+import sys
+
+from flask import Flask, request, Response
+from botbuilder.core import (
+    BotFrameworkAdapter,
+    BotFrameworkAdapterSettings,
+    ConversationState,
+    MemoryStorage,
+    TurnContext,
+    UserState,
+)
+from botbuilder.schema import Activity
+
+from dialogs import UserProfileDialog
+from bots import DialogBot
+
+LOOP = asyncio.get_event_loop()
+APP = Flask(__name__, instance_relative_config=True)
+APP.config.from_object("config.DefaultConfig")
+
+SETTINGS = BotFrameworkAdapterSettings(
+    APP.config["APP_ID"], APP.config["APP_PASSWORD"]
+)
+ADAPTER = BotFrameworkAdapter(SETTINGS)
+
+
+# Catch-all for errors.
+async def on_error(context: TurnContext, error: Exception):
+    # This check writes out errors to console log
+    # NOTE: In production environment, you should consider logging this to Azure
+    #       application insights.
+    print(f"\n [on_turn_error]: { error }", file=sys.stderr)
+    # Send a message to the user
+    await context.send_activity("Oops. Something went wrong!")
+    # Clear out state
+    await CONVERSATION_STATE.delete(context)
+
+
+ADAPTER.on_turn_error = on_error
+
+# Create MemoryStorage, UserState and ConversationState
+MEMORY = MemoryStorage()
+CONVERSATION_STATE = ConversationState(MEMORY)
+USER_STATE = UserState(MEMORY)
+
+# create main dialog and bot
+DIALOG = UserProfileDialog(USER_STATE)
+BOT = DialogBot(CONVERSATION_STATE, USER_STATE, DIALOG)
+
+
+@APP.route("/api/messages", methods=["POST"])
+def messages():
+    """Main bot message handler."""
+    if "application/json" in request.headers["Content-Type"]:
+        body = request.json
+    else:
+        return Response(status=415)
+
+    activity = Activity().deserialize(body)
+    auth_header = (
+        request.headers["Authorization"] if "Authorization" in request.headers else ""
+    )
+
+    async def aux_func(turn_context):
+        await BOT.on_turn(turn_context)
+
+    try:
+        task = LOOP.create_task(
+            ADAPTER.process_activity(activity, auth_header, aux_func)
+        )
+        LOOP.run_until_complete(task)
+        return Response(status=201)
+    except Exception as exception:
+        raise exception
+
+
+if __name__ == "__main__":
+    try:
+        APP.run(debug=False, port=APP.config["PORT"])  # nosec debug
+    except Exception as exception:
+        raise exception
