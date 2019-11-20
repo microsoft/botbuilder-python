5090f955723ca6f6d973fa0c6eccbd72ca90b032
virtual-josh <ratliff.joshua@gmail.com>
2019-11-20 14:59:06 -0800

    adding mention bot
    cleaning up linter

--- libraries/botbuilder-core/tests/teams/mentions/bots/__init__.py
+++ libraries/botbuilder-core/tests/teams/mentions/bots/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from .mentions_bot import MentionBot
+from .mention_bot import MentionBot
 
 __all__ = ["MentionBot"]
