6d7cff171c0008e39ce8126b733d218bbdd479a3
mdrichardson <v-micric@microsoft.com>
2019-11-22 11:59:21 -0800

    _dialog_stack to dialog_stack

--- libraries/botbuilder-dialogs/botbuilder/dialogs/dialog_state.py
+++ libraries/botbuilder-dialogs/botbuilder/dialogs/dialog_state.py
@@ -8,15 +8,11 @@ from .dialog_instance import DialogInstance
 class DialogState:
     def __init__(self, stack: List[DialogInstance] = None):
         if stack is None:
-            self._dialog_stack = []
+            self.dialog_stack = []
         else:
-            self._dialog_stack = stack
-
-    @property
-    def dialog_stack(self):
-        return self._dialog_stack
+            self.dialog_stack = stack
 
     def __str__(self):
-        if not self._dialog_stack:
+        if not self.dialog_stack:
             return "dialog stack empty!"
-        return " ".join(map(str, self._dialog_stack))
+        return " ".join(map(str, self.dialog_stack))
