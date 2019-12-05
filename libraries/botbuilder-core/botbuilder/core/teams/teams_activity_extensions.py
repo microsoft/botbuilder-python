6bff52f1f281e55f876888574acaea5481038453
virtual-josh <ratliff.joshua@gmail.com>
2019-12-05 14:11:57 -0800

    black

--- libraries/botbuilder-core/botbuilder/core/teams/teams_activity_extensions.py
+++ libraries/botbuilder-core/botbuilder/core/teams/teams_activity_extensions.py
@@ -1,8 +1,10 @@
 from botbuilder.schema import Activity
 from botbuilder.schema.teams import NotificationInfo, TeamsChannelData, TeamInfo
 
+
 def dummy():
     return 1
+
 
 def teams_get_channel_id(activity: Activity) -> str:
     if not activity:
