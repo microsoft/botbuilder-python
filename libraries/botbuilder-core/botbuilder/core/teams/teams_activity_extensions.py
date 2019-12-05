0cb6195e0bc40f50a4b360c88a4226b850f42657
virtual-josh <ratliff.joshua@gmail.com>
2019-12-05 13:57:46 -0800

    kicking off new build

--- libraries/botbuilder-core/botbuilder/core/teams/teams_activity_extensions.py
+++ libraries/botbuilder-core/botbuilder/core/teams/teams_activity_extensions.py
@@ -1,11 +1,12 @@
 from botbuilder.schema import Activity
 from botbuilder.schema.teams import NotificationInfo, TeamsChannelData, TeamInfo
 
+def dummy():
+    return 1
 
 def teams_get_channel_id(activity: Activity) -> str:
     if not activity:
         return None
-    
 
     if activity.channel_data:
         channel_data = TeamsChannelData().deserialize(activity.channel_data)
