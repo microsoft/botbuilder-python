e82b4c0ec20d0539c82c102eb138115834cd7b7c
Eric Dahlvang <v-eridah@microsoft.com>
2019-12-13 17:03:30 -0800

    add crude action based fetch task scenario

---
+++ scenarios/action-based-messaging-extension-fetch-task/bots/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from .action_based_messaging_extension_fetch_task_bot import ActionBasedMessagingExtensionFetchTaskBot
+
+__all__ = ["ActionBasedMessagingExtensionFetchTaskBot"]
