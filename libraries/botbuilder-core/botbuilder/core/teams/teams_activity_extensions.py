9a5db0fcbaa70ed03cfed686cba586c38a88b0ee
virtual-josh <ratliff.joshua@gmail.com>
2019-12-13 16:01:56 -0800

    adding copyright

--- libraries/botbuilder-core/botbuilder/core/teams/teams_activity_extensions.py
+++ libraries/botbuilder-core/botbuilder/core/teams/teams_activity_extensions.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 from botbuilder.schema import Activity
 from botbuilder.schema.teams import NotificationInfo, TeamsChannelData, TeamInfo
 
--- libraries/botbuilder-core/botbuilder/core/teams/teams_helper.py
+++ libraries/botbuilder-core/botbuilder/core/teams/teams_helper.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 from inspect import getmembers
 from typing import Type
 from enum import Enum
