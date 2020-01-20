8680347629eb53700076ccd14970870afc8446c1
Emily Olshefski <emilygraceolshefski@gmail.com>
2020-01-20 11:23:01 -0800

    Fixed formatting

--- libraries/botbuilder-dialogs/botbuilder/dialogs/dialog_reason.py
+++ libraries/botbuilder-dialogs/botbuilder/dialogs/dialog_reason.py
@@ -9,17 +9,17 @@ NOTE: Multiple formats added, will remove whatever formatting isn't needed
 class DialogReason(Enum):
     """ Indicates in which a dialog-related method is being called.
     :var BeginCalled: A dialog is being started through a call to `DialogContext.begin()`.
-    :vartype BeginCalled: :class:`int`
+    :vartype BeginCalled: int
     :var ContinuCalled: A dialog is being continued through a call to `DialogContext.continue_dialog()`.
-    :vartype ContinueCalled: i:class:`int`
+    :vartype ContinueCalled: int
     :var EndCalled: A dialog ended normally through a call to `DialogContext.end_dialog()
-    :vartype EndCalled: :class:`int`
+    :vartype EndCalled: int
     :var ReplaceCalled: A dialog is ending because it's being replaced through a call to `DialogContext.replace_dialog()`.
-    :vartype ReplacedCalled: :class:`int`
+    :vartype ReplacedCalled: int
     :var CancelCalled: A dialog was cancelled as part of a call to `DialogContext.cancel_all_dialogs()`.
-    :vartype CancelCalled: :class:`int`
+    :vartype CancelCalled: int
     :var NextCalled: A preceding step was skipped through a call to `WaterfallStepContext.next()`.
-    :vartype NextCalled: :class:`int`
+    :vartype NextCalled: int
     """
     
     """
