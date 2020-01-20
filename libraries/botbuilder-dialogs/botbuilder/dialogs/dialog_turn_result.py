a136b9a2f93d0eebb481e78abe501c597b6d1120
Emily Olshefski <v-emolsh@microsoft.com>
2020-01-20 10:46:14 -0800

    emolsh/api-ref-docs-dialogturnresult

--- libraries/botbuilder-dialogs/botbuilder/dialogs/dialog_turn_result.py
+++ libraries/botbuilder-dialogs/botbuilder/dialogs/dialog_turn_result.py
@@ -3,16 +3,41 @@
 
 from .dialog_turn_status import DialogTurnStatus
 
-
 class DialogTurnResult:
+    """ 
+    Result returned to the caller of one of the various stack manipulation methods.
+    """
     def __init__(self, status: DialogTurnStatus, result: object = None):
+    """
+    :param status: The current status of the stack.
+    :type status: :class:`DialogTurnStatus`
+    :param result: The result returned by a dialog that was just ended.
+    :type result: object
+    """
         self._status = status
         self._result = result
-
+        
     @property
     def status(self):
-        return self._status
+    """
+    Gets or sets the current status of the stack.
 
+    :return self._status:
+    :rtype self._status: :class:`DialogTurnStatus`
+
+    """
+        return self._status
+        
+    """
+    Final result returned by a dialog that just completed.
+        ..remarks:
+             This will only be populated in certain cases:
+             - The bot calls `DialogContext.begin_dialog()` to start a new dialog and the dialog ends immediately.
+             - The bot calls `DialogContext.continue_dialog()` and a dialog that was active ends.
+
+    :return self._result: 
+    :rtype self._result: object
+    """
     @property
     def result(self):
         return self._result
