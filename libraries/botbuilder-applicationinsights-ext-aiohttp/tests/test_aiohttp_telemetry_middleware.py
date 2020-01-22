24be56403e331a609560e0d1c77469156c54a087
Axel Suarez <axsuarez@microsoft.com>
2020-01-21 18:46:24 -0800

    created separate package

---
+++ libraries/botbuilder-applicationinsights-ext-aiohttp/tests/test_aiohttp_telemetry_middleware.py
@@ -0,0 +1,8 @@
+from aiounittest import AsyncTestCase
+
+from botbuilder.applicationinsights.aiohttp import bot_telemetry_middleware
+
+
+class TestAiohttpTelemetryMiddleware(AsyncTestCase):
+    async def test_bot_telemetry_middleware(self):
+        pass
