f9fba8e622cb865cb474192a71611fe2a5b5d4d6
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-04-28 09:45:08 -0500

    Added HealthCheck

--- libraries/botbuilder-core/botbuilder/core/__init__.py
+++ libraries/botbuilder-core/botbuilder/core/__init__.py
@@ -42,6 +42,7 @@ from .user_state import UserState
 from .user_token_provider import UserTokenProvider
 from .register_class_middleware import RegisterClassMiddleware
 from .adapter_extensions import AdapterExtensions
+from .healthcheck import HealthCheck
 
 __all__ = [
     "ActivityHandler",
@@ -63,6 +64,7 @@ __all__ = [
     "ConversationState",
     "conversation_reference_extension",
     "ExtendedUserTokenProvider",
+    "HealthCheck",
     "IntentScore",
     "InvokeResponse",
     "MemoryStorage",
