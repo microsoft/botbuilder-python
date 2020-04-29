75e7f188af5d11e6f56ac015d454f18f584c585c
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-04-29 17:08:35 -0500

    Added QnA Low score tests

--- libraries/botbuilder-ai/tests/qna/test_qna.py
+++ libraries/botbuilder-ai/tests/qna/test_qna.py
@@ -811,6 +811,54 @@ class QnaApplicationTest(aiounittest.AsyncTestCase):
             )
             self.assertEqual(True, results[0].score < 1, "Score should be low.")
 
+    async def test_low_score_variation(self):
+        qna = QnAMaker(QnaApplicationTest.tests_endpoint)
+        options = QnAMakerOptions(top=5, context=None)
+
+        turn_context = QnaApplicationTest._get_context("Q11", TestAdapter())
+        response_json = QnaApplicationTest._get_json_for_file(
+            "QnaMaker_TopNAnswer.json"
+        )
+
+        # active learning enabled
+        with patch(
+            "aiohttp.ClientSession.post",
+            return_value=aiounittest.futurized(response_json),
+        ):
+            results = await qna.get_answers(turn_context, options)
+            self.assertIsNotNone(results)
+            self.assertEqual(
+                4, len(results), "should get four results"
+            )
+
+            filtered_results = qna.get_low_score_variation(results)
+            self.assertIsNotNone(filtered_results)
+            self.assertEqual(
+                3, len(filtered_results), "should get three results"
+            )
+
+        # active learning disabled
+        turn_context = QnaApplicationTest._get_context("Q11", TestAdapter())
+        response_json = QnaApplicationTest._get_json_for_file(
+            "QnaMaker_TopNAnswer_DisableActiveLearning.json"
+        )
+
+        with patch(
+            "aiohttp.ClientSession.post",
+            return_value=aiounittest.futurized(response_json),
+        ):
+            results = await qna.get_answers(turn_context, options)
+            self.assertIsNotNone(results)
+            self.assertEqual(
+                4, len(results), "should get four results"
+            )
+
+            filtered_results = qna.get_low_score_variation(results)
+            self.assertIsNotNone(filtered_results)
+            self.assertEqual(
+                3, len(filtered_results), "should get three results"
+            )
+
     @classmethod
     async def _get_service_result(
         cls,
