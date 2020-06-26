8996b62f1801c87f1f1ff6929e9127c7f84f1ad0
Emily Olshefski <emilygraceolshefski@gmail.com>
2020-06-26 09:59:41 -0700

    Update null_telemetry_client.py

--- libraries/botbuilder-core/botbuilder/core/null_telemetry_client.py
+++ libraries/botbuilder-core/botbuilder/core/null_telemetry_client.py
@@ -118,7 +118,7 @@ class NullTelemetryClient(BotTelemetryClient):
         :param url: The actual URL for this request (to show in individual request instances).
         :param success: True if the request ended in success, False otherwise.
         :param start_time: the start time of the request. The value should look the same as the one returned \
-        by :func:`datetime.isoformat` (defaults to: None)
+        by :func:`datetime.isoformat`. (defaults to: None)
         :param duration: the number of milliseconds that this request lasted. (defaults to: None)
         :param response_code: the response code that this request returned. (defaults to: None)
         :param http_method: the HTTP method that triggered this request. (defaults to: None)
