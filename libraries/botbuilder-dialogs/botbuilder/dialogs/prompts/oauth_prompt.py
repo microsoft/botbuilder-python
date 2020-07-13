45efb71bc501a222ee91c40bd8cf6bdd403f3c23
Carlos Castro <carlosscastro@users.noreply.github.com>
2020-07-13 15:06:33 -0700

    [Patch] SSO + Teams: maintain sign in link for channels that require it (#1245)
    * SSO + Teams: maintain sign in link for channels that require it
    
    * Fix syntax :)
    
    * OAuthPrompt: Formatting
    
    Co-authored-by: Axel Suarez <axsuarez@microsoft.com>

--- libraries/botbuilder-dialogs/botbuilder/dialogs/prompts/oauth_prompt.py
+++ libraries/botbuilder-dialogs/botbuilder/dialogs/prompts/oauth_prompt.py
@@ -362,7 +362,9 @@ class OAuthPrompt(Dialog):
                 ):
                     if context.activity.channel_id == Channels.emulator:
                         card_action_type = ActionTypes.open_url
-                else:
+                elif not OAuthPrompt._channel_requires_sign_in_link(
+                    context.activity.channel_id
+                ):
                     link = None
 
                 json_token_ex_resource = (
@@ -616,6 +618,13 @@ class OAuthPrompt(Dialog):
             return False
 
         return True
+
+    @staticmethod
+    def _channel_requires_sign_in_link(channel_id: str) -> bool:
+        if channel_id in [Channels.ms_teams]:
+            return True
+
+        return False
 
     @staticmethod
     def _is_token_exchange_request_invoke(context: TurnContext) -> bool:
