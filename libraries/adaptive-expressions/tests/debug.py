dc0a5c0e8ecc3c7736f544b067ca591184a233bd
Fei Chen <feich@microsoft.com>
2020-07-31 17:14:59 +0800

    remove debug.py

--- libraries/adaptive-expressions/tests/debug.py
+++
@@ -1,9 +0,0 @@
-from adaptive.expressions import Expression
-
-scope = {"bag": {"three": 3.0}, "items": ["zero", "one", "two"]}
-
-parsed = Expression.parse("contains(items, 'zero')")
-
-value, error = parsed.try_evaluate(scope)
-
-print(value)
