c34e994470bb8e1be6bbb9a4b1d0565a10f76337
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-08-04 11:28:08 -0500

    pylint correction

--- libraries/botbuilder-dialogs/botbuilder/dialogs/prompts/activity_prompt.py
+++ libraries/botbuilder-dialogs/botbuilder/dialogs/prompts/activity_prompt.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from abc import ABC
 from typing import Callable, Dict
 
 from botbuilder.core import TurnContext
