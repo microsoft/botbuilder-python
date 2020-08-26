a0a26d651575e383c00cc8f0e07d9842bf49f988
Eric Dahlvang <ericdahlvang@gmail.com>
2020-08-25 19:27:30 -0700

    Add installation update sub events to ActivityHandler

--- libraries/botbuilder-core/tests/test_activity_handler.py
+++ libraries/botbuilder-core/tests/test_activity_handler.py
@@ -77,6 +77,14 @@ class TestingActivityHandler(ActivityHandler):
         self.record.append("on_installation_update")
         return await super().on_installation_update(turn_context)
 
+    async def on_installation_update_add_activity(self, turn_context: TurnContext):
+        self.record.append("on_installation_update_add_activity")
+        return await super().on_installation_update_add_activity(turn_context)
+
+    async def on_installation_update_remove_activity(self, turn_context: TurnContext):
+        self.record.append("on_installation_update_remove_activity")
+        return await super().on_installation_update_remove_activity(turn_context)
+
     async def on_unrecognized_activity_type(self, turn_context: TurnContext):
         self.record.append("on_unrecognized_activity_type")
         return await super().on_unrecognized_activity_type(turn_context)
@@ -245,6 +253,34 @@ class TestActivityHandler(aiounittest.AsyncTestCase):
 
         assert len(bot.record) == 1
         assert bot.record[0] == "on_installation_update"
+
+    async def test_on_installation_update_add_activity(self):
+        activity = Activity(type=ActivityTypes.installation_update, action="add")
+
+        adapter = TestInvokeAdapter()
+        turn_context = TurnContext(adapter, activity)
+
+        # Act
+        bot = TestingActivityHandler()
+        await bot.on_turn(turn_context)
+
+        assert len(bot.record) == 2
+        assert bot.record[0] == "on_installation_update"
+        assert bot.record[1] == "on_installation_update_add_activity"
+
+    async def test_on_installation_update_add_remove_activity(self):
+        activity = Activity(type=ActivityTypes.installation_update, action="remove")
+
+        adapter = TestInvokeAdapter()
+        turn_context = TurnContext(adapter, activity)
+
+        # Act
+        bot = TestingActivityHandler()
+        await bot.on_turn(turn_context)
+
+        assert len(bot.record) == 2
+        assert bot.record[0] == "on_installation_update"
+        assert bot.record[1] == "on_installation_update_remove_activity"
 
     async def test_healthcheck(self):
         activity = Activity(type=ActivityTypes.invoke, name="healthcheck",)
