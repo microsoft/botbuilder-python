4504cbf125a4516d5d0bf4c7953db89bfd917af4
Eric Dahlvang <EricDahlvang@gmail.com>
2020-09-03 16:36:01 -0700

    Update jwt_token_validation.py

--- libraries/botframework-connector/botframework/connector/auth/jwt_token_validation.py
+++ libraries/botframework-connector/botframework/connector/auth/jwt_token_validation.py
@@ -166,7 +166,7 @@ class JwtTokenValidation:
         if auth_config and auth_config.claims_validator:
             await auth_config.claims_validator(claims)
         elif SkillValidation.is_skill_claim(claims):
-            # Skill claims must be validated using AuthenticationConfiguration validateClaims
+            # Skill claims must be validated using AuthenticationConfiguration claims_validator
             raise PermissionError("Unauthorized Access. Request is not authorized. Skill Claims require validation.")
 
     @staticmethod
