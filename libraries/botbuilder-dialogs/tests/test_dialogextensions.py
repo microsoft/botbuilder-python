2b18086e5e8169e7a8e47ef9a76ae3e9cdb723b1
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-10-07 10:39:54 -0500

    Add EndOfConversationCodes to EndOfConversation activity from Skill

--- libraries/botbuilder-dialogs/tests/test_dialogextensions.py
+++ libraries/botbuilder-dialogs/tests/test_dialogextensions.py
@@ -7,6 +7,7 @@ import uuid
 
 import aiounittest
 
+from botframework.connector.auth import ClaimsIdentity, AuthenticationConstants
 from botbuilder.core import (
     TurnContext,
     MessageFactory,
@@ -28,8 +29,7 @@ from botbuilder.core.transcript_logger import (
     TranscriptLoggerMiddleware,
     ConsoleTranscriptLogger,
 )
-from botbuilder.schema import ActivityTypes, Activity
-from botframework.connector.auth import ClaimsIdentity, AuthenticationConstants
+from botbuilder.schema import ActivityTypes, Activity, EndOfConversationCodes
 from botbuilder.dialogs import (
     ComponentDialog,
     TextPrompt,
@@ -111,6 +111,7 @@ class DialogExtensionsTests(aiounittest.AsyncTestCase):
                 self.eoc_sent, "Skills should send EndConversation to channel"
             )
             assert ActivityTypes.end_of_conversation == self.eoc_sent.type
+            assert EndOfConversationCodes.completed_successfully == self.eoc_sent.code
             assert self.eoc_sent.value == "SomeName"
         else:
             self.assertIsNone(
