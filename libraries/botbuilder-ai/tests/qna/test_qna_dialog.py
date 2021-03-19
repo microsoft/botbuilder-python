68e4aaa85e1718f07a1c354bcfc6c01b9f00eb1b
Ashley Finafrock <v-asho@microsoft.com>
2021-03-19 09:13:43 -0700

    Refactor multiturn; started active learning test

--- libraries/botbuilder-ai/tests/qna/test_qna_dialog.py
+++ libraries/botbuilder-ai/tests/qna/test_qna_dialog.py
@@ -9,14 +9,7 @@ from botbuilder.ai.qna.dialogs import QnAMakerDialog
 from botbuilder.schema import Activity, ActivityTypes
 from botbuilder.core import ConversationState, MemoryStorage, TurnContext
 from botbuilder.core.adapters import TestAdapter, TestFlow
-from botbuilder.dialogs import (
-    # Dialog,
-    DialogSet,
-    # WaterfallDialog,
-    # WaterfallStepContext,
-    # DialogTurnResult,
-    DialogTurnStatus,
-)
+from botbuilder.dialogs import DialogSet, DialogTurnStatus
 
 class QnaMakerDialogTest(aiounittest.AsyncTestCase):
     # Note this is NOT a real QnA Maker application ID nor a real QnA Maker subscription-key
@@ -26,13 +19,12 @@ class QnaMakerDialogTest(aiounittest.AsyncTestCase):
     _endpoint_key: str = "1k997n7w-207z-36p3-j2u1-09tas20ci6011"
     _host: str = "https://dummyqnahost.azurewebsites.net/qnamaker"
 
-    _tell_me_about_birds_text: str = "Tell me about birds"
-    _choose_bird_text: str = "Choose one of the following birds to get more info"
-    _bald_eagle_text: str = "Bald Eagle"
-    _hummingbird_text: str = "Hummingbird"
-    _not_bald_text: str = "Apparently these guys aren't actually bald!"
+    _tell_me_about_birds: str = "Tell me about birds"
+    _bald_eagle: str = "Bald Eagle"
+    _esper: str = "Esper"
 
-    async def test_multiturn_qna_dialog(self):
+    async def test_multiturn_dialog(self):
+        # Set Up QnAMakerDialog
         convo_state = ConversationState(MemoryStorage())
         dialog_state = convo_state.create_property("dialogState")
         dialogs = DialogSet(dialog_state)
@@ -44,7 +36,8 @@ class QnaMakerDialogTest(aiounittest.AsyncTestCase):
         )
         dialogs.add(qna_dialog)
 
-        async def execute_qna_dialog_test(turn_context: TurnContext) -> None:
+        # Callback that runs the dialog
+        async def execute_qna_dialog(turn_context: TurnContext) -> None:
             if (turn_context.activity.type != ActivityTypes.message):
                 raise TypeError('Failed to execute QnA dialog. Should have received a message activity.')
 
@@ -61,10 +54,10 @@ class QnaMakerDialogTest(aiounittest.AsyncTestCase):
 
                 await convo_state.save_changes(turn_context)
 
-        adapt = TestAdapter(execute_qna_dialog_test)
-
-        test_flow = TestFlow(None, adapt)
-        tf2 = await test_flow.send(self._tell_me_about_birds_text)
+        # Send and receive messages from QnA dialog
+        test_adapter = TestAdapter(execute_qna_dialog)
+        test_flow = TestFlow(None, test_adapter)
+        tf2 = await test_flow.send(self._tell_me_about_birds)
         dialog_reply: Activity = tf2.adapter.activity_buffer[0]
         self.assertIsNotNone(dialog_reply)
         self.assertIsInstance(dialog_reply, Activity)
@@ -73,23 +66,62 @@ class QnaMakerDialogTest(aiounittest.AsyncTestCase):
         self.assertEqual(len(attachments), 1)
         buttons = attachments[0].content.buttons
         self.assertEqual(
-            len(buttons), 2, 'Should have only received 2 buttons in multi-turn prompt'
+            len(buttons), 2, "Should have only received 2 buttons in multi-turn prompt"
         )
-        self.assertEqual(buttons[0].value, self._bald_eagle_text)
-        self.assertEqual(buttons[1].value, self._hummingbird_text)
-        tf3 = await tf2.assert_reply(self._choose_bird_text)
-        tf4 = await tf3.send(self._bald_eagle_text)
-        await tf4.assert_reply(self._not_bald_text)
+        self.assertEqual(buttons[0].value, self._bald_eagle)
+        self.assertEqual(buttons[1].value, "Hummingbird")
+        tf3 = await tf2.assert_reply("Choose one of the following birds to get more info")
+        tf4 = await tf3.send("Bald Eagle")
+        await tf4.assert_reply("Apparently these guys aren't actually bald!")
     
+    async def test_active_learning(self):
+        # Set Up QnAMakerDialog
+        convo_state = ConversationState(MemoryStorage())
+        dialog_state = convo_state.create_property("dialogState")
+        dialogs = DialogSet(dialog_state)
+
+        qna_dialog = QnAMakerDialog(
+            self._knowledge_base_id,
+            self._endpoint_key,
+            self._host
+        )
+        dialogs.add(qna_dialog)
+
+        # Callback that runs the dialog
+        async def execute_qna_dialog(turn_context: TurnContext) -> None:
+            if (turn_context.activity.type != ActivityTypes.message):
+                raise TypeError('Failed to execute QnA dialog. Should have received a message activity.')
+
+            response_json = QnaMakerDialogTest._get_json_res(turn_context.activity.text)
+            dialog_context = await dialogs.create_context(turn_context)
+            with patch(
+                "aiohttp.ClientSession.post",
+                return_value=aiounittest.futurized(response_json),
+            ):
+                results = await dialog_context.continue_dialog()
+
+                if results.status == DialogTurnStatus.Empty:
+                    await dialog_context.begin_dialog("QnAMakerDialog")
+
+                await convo_state.save_changes(turn_context)
+
+        # Send and receive messages from QnA dialog
+        test_adapter = TestAdapter(execute_qna_dialog)
+        test_flow = TestFlow(None, test_adapter)
+        tf2 = await test_flow.send(self._esper)
+        print(tf2)
+
     @classmethod
     def _get_json_res(cls, text: str) -> object:
-        if (text == cls._tell_me_about_birds_text):
+        if (text == cls._tell_me_about_birds):
             return QnaMakerDialogTest._get_json_for_file("QnAMakerDialog_MultiTurn_Answer1.json")
         
-        if (text == cls._bald_eagle_text):
-            # file_name = cls._not_bald_text
+        if (text == cls._bald_eagle):
             return QnaMakerDialogTest._get_json_for_file("QnAMakerDialog_MultiTurn_Answer2.json")
         
+        if (text == cls._esper):
+            return QnaMakerDialogTest._get_json_for_file("QnAMakerDialog_ActiveLearning.json")
+
         return None
 
     @staticmethod
