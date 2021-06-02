5951be7b9010002c302a83fbce16fd46b1dbb2b1
sw-joelmut <joel.mut@southworks.com>
2021-06-02 17:51:28 -0300

    Add copyright licence

--- libraries/botbuilder-core/tests/skills/test_skill_conversation_id_factory.py
+++ libraries/botbuilder-core/tests/skills/test_skill_conversation_id_factory.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 from uuid import uuid4 as uuid
 from aiounittest import AsyncTestCase
 from botbuilder.core import MemoryStorage
--- libraries/botbuilder-core/tests/skills/test_skill_handler.py
+++ libraries/botbuilder-core/tests/skills/test_skill_handler.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import hashlib
 import json
 from datetime import datetime
