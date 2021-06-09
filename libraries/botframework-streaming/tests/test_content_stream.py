c6ff9053e5df2260f7554a5dbeb3b58a0b48793b
Axel Suarez <axsuarez@microsoft.com>
2021-06-09 14:07:41 -0700

    test send operations

--- libraries/botframework-streaming/tests/test_content_stream.py
+++ libraries/botframework-streaming/tests/test_content_stream.py
@@ -8,18 +8,19 @@ import aiounittest
 from botframework.streaming.payloads import ContentStream
 from botframework.streaming.payloads.assemblers import PayloadStreamAssembler
 
+
 class TestResponses(aiounittest.AsyncTestCase):
     async def test_content_stream_ctor_none_assembler_throws(self):
         with self.assertRaises(TypeError):
             sut = ContentStream(uuid4(), None)
-    
+
     async def test_content_stream_id(self):
         test_id = uuid4()
         test_assembler = PayloadStreamAssembler(None, test_id)
         sut = ContentStream(test_id, test_assembler)
 
         self.assertEqual(test_id, sut.identifier)
-    
+
     async def test_content_stream_type(self):
         test_id = uuid4()
         test_assembler = PayloadStreamAssembler(None, test_id)
