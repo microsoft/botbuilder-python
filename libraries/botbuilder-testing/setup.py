8d7198039c0f9ced4bd902ad1b2af2bb9a0761f9
tracyboehrer <tracyboehrer@users.noreply.github.com>
2021-07-01 13:56:29 -0500

    Version to 4.15.0 (#1761)
    * Version to 4.15.0
    
    * Updated version in README

--- libraries/botbuilder-testing/setup.py
+++ libraries/botbuilder-testing/setup.py
@@ -5,10 +5,10 @@ import os
 from setuptools import setup
 
 REQUIRES = [
-    "botbuilder-schema==4.14.0",
-    "botbuilder-core==4.14.0",
-    "botbuilder-dialogs==4.14.0",
-    "botbuilder-azure==4.14.0",
+    "botbuilder-schema==4.15.0",
+    "botbuilder-core==4.15.0",
+    "botbuilder-dialogs==4.15.0",
+    "botbuilder-azure==4.15.0",
     "pytest~=6.2.3",
 ]
 
