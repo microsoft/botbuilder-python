ddcf958b940bf2646ca37b6085e943f557f9a929
Bruce Haley <v-brucehaley@microsoft.com>
2022-05-03 16:13:06 -0700

    vulnerability fix: "django==2.2.28"

--- libraries/botbuilder-applicationinsights/setup.py
+++ libraries/botbuilder-applicationinsights/setup.py
@@ -12,7 +12,7 @@ REQUIRES = [
 ]
 TESTS_REQUIRES = [
     "aiounittest==1.3.0",
-    "django==2.2.24",  # For samples
+    "django==2.2.28",  # For samples
     "djangorestframework==3.10.3",  # For samples
     "flask==1.1.1",  # For samples
 ]
