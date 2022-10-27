45cfdb238fb8972fcd8a678fd0e7eec0b4bf4480
Axel Suarez Martinez <axsuarez@microsoft.com>
2022-10-27 15:00:15 -0700

    Adding deprectaion message and removing an experimental test

--- libraries/botbuilder-schema/botbuilder/schema/__init__.py
+++ libraries/botbuilder-schema/botbuilder/schema/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+from warnings import warn
+
 from ._models_py3 import Activity
 from ._models_py3 import ActivityEventNames
 from ._models_py3 import AdaptiveCardInvokeAction
@@ -74,6 +76,10 @@ from ._connector_client_enums import (
 from ._sign_in_enums import SignInConstants
 from .callerid_constants import CallerIdConstants
 from .speech_constants import SpeechConstants
+
+warn(
+    "The Bot Framework Python SDK is being retired with final long-term support ending in November 2023, after which this repository will be archived. There will be no further feature development, with only critical security and bug fixes within this repository being undertaken. Existing bots built with this SDK will continue to function. For all new bot development we recommend that you adopt Power Virtual Agents."
+)
 
 __all__ = [
     "Activity",
