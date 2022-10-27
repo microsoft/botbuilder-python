c0bb7624e904f9af4f48ee21af820b40883f88de
Tracy Boehrer <trboehre@microsoft.com>
2022-10-27 15:39:48 -0500

    Corrected streaming dependencies

--- libraries/botframework-streaming/setup.py
+++ libraries/botframework-streaming/setup.py
@@ -6,8 +6,8 @@ from setuptools import setup
 
 VERSION = os.environ["packageVersion"] if "packageVersion" in os.environ else "4.15.0"
 REQUIRES = [
-    "botbuilder-schema>=4.12.0",
-    "botframework-connector>=4.12.0",
+    "botbuilder-schema==4.15.0",
+    "botframework-connector==4.15.0",
 ]
 
 root = os.path.abspath(os.path.dirname(__file__))
