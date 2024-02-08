004a286b6bd042c56f33a9c77d1eee3a5c8f3fb2
tracyboehrer <tracyboehrer@users.noreply.github.com>
2024-02-08 13:31:22 -0600

    aiohttp to 3.9.3 (#2069)
    * aiohttp to 3.9.2
    
    * aiohttp to 3.9.3
    
    * Update botbuilder-python-ci.yml for Azure Pipelines
    
    Changed Python build versions
    
    * Updated README
    
    ---------
    
    Co-authored-by: Tracy Boehrer <trboehre@microsoft.com>

--- libraries/botbuilder-integration-applicationinsights-aiohttp/setup.py
+++ libraries/botbuilder-integration-applicationinsights-aiohttp/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 REQUIRES = [
     "applicationinsights>=0.11.9",
-    "aiohttp==3.8.5",
+    "aiohttp==3.9.3",
     "botbuilder-schema==4.15.0",
     "botframework-connector==4.15.0",
     "botbuilder-core==4.15.0",
