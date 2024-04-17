2407c51698a7ab3a81851e2eeafb4624c2c06cc9
tracyboehrer <tracyboehrer@users.noreply.github.com>
2024-04-17 09:59:05 -0500

    Updated generators to use CloudAdapter and latest version (#2094)
    * Updated generators to use CloudAdapter and latest version
    
    * Updated version and added missing SingleTenant config items
    
    ---------
    
    Co-authored-by: Tracy Boehrer <trboehre@microsoft.com>

--- generators/app/templates/core/{{cookiecutter.bot_name}}/app.py
+++ generators/app/templates/core/{{cookiecutter.bot_name}}/app.py
@@ -8,16 +8,16 @@ This sample shows how to create a bot that demonstrates the following:
 - Handle user interruptions for such things as `Help` or `Cancel`.
 - Prompt for and validate requests for information from the user.
 """
-
+from http import HTTPStatus
 from aiohttp import web
 from aiohttp.web import Request, Response, json_response
 from botbuilder.core import (
-    BotFrameworkAdapterSettings,
     ConversationState,
     MemoryStorage,
     UserState,
 )
 from botbuilder.core.integration import aiohttp_error_middleware
+from botbuilder.integration.aiohttp import ConfigurationBotFrameworkAuthentication
 from botbuilder.schema import Activity
 
 from config import DefaultConfig
@@ -31,7 +31,7 @@ CONFIG = DefaultConfig()
 
 # Create adapter.
 # See https://aka.ms/about-bot-adapter to learn more about how bots work.
-SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
+SETTINGS = ConfigurationBotFrameworkAuthentication(CONFIG)
 
 # Create MemoryStorage, UserState and ConversationState
 MEMORY = MemoryStorage()
@@ -49,21 +49,21 @@ DIALOG = MainDialog(RECOGNIZER, BOOKING_DIALOG)
 BOT = DialogAndWelcomeBot(CONVERSATION_STATE, USER_STATE, DIALOG)
 
 
-# Listen for incoming requests on /api/messages.
+# Listen for incoming requests on /api/messages
 async def messages(req: Request) -> Response:
     # Main bot message handler.
     if "application/json" in req.headers["Content-Type"]:
         body = await req.json()
     else:
-        return Response(status=415)
+        return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
 
     activity = Activity().deserialize(body)
     auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
 
-    response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+    response = await ADAPTER.process_activity(auth_header, activity, BOT.on_turn)
     if response:
         return json_response(data=response.body, status=response.status)
-    return Response(status=201)
+    return Response(status=HTTPStatus.OK)
 
 
 APP = web.Application(middlewares=[aiohttp_error_middleware])
